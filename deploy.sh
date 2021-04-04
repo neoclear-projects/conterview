9f00a1a41143d4310c1b2eaef2c5d32fbc369051
NeoClear <neoclear@outlook.com>
2021-04-04 15:31:25 -0400

    reorder deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
-
+docker-compose down
 docker stop code
 docker rm $(docker ps -a -q)
 docker run -d --rm --name code -ti neoclear/conterview-code-engine
-docker-compose down
 docker-compose up -d --build
