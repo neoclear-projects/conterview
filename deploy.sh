dfca0a6441785448f2db3b4b28ee685a2ce559ef
NeoClear <neoclear@outlook.com>
2021-04-04 17:30:55 -0400

    improve build script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
+docker-compose build
 docker-compose down
 docker stop code
 docker rm $(docker ps -a -q)
 docker run -d --rm --name code -ti neoclear/conterview-code-engine
-docker-compose up -d --build
+docker-compose up -d
