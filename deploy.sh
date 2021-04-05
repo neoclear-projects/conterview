406c83299f7208582f280c5a1b9500809f800c10
NeoClear <neoclear@outlook.com>
2021-04-05 19:13:05 -0400

    update script to reduce burden on system memory

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-docker-compose build
 docker-compose down
+docker-compose build
 docker stop code
 docker rm $(docker ps -a -q)
 docker run -d --rm --name code -ti neoclear/conterview-code-engine
